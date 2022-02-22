76877b378f7144572a2ff38463132f16148a4847
lucastimho <lucas.t.ho.lh@gmail.com>
2022-02-22 11:43:29 -0600

    adds login alert

--- src/Layout.js
+++ src/Layout.js
@@ -1,7 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import { Outlet, Link } from "react-router-dom";
 
-const Layout = () => {
-  return <></>;
+const Layout = () => {};
+
+const isLoggedIn = () => {
+  if (localStorage.getItem("jwt")) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const loginAlert = () => {
+  if (isLoggedIn()) {
+    return (
+      <>
+        <strong>Sucessfully Logged In!</strong>
+        You are free to make posts now.
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </>
+    );
+  }
 };
 
 export default Layout;
+
+ReactDOM.render(loginAlert, document.getElementsByClassName("alert alert-success alert-dismissible fade show"));
