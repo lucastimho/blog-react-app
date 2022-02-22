8c82bbf16d1e615f32678df4bdb146d7aa392823
lucastimho <lucas.t.ho.lh@gmail.com>
2022-02-21 23:29:36 -0600

    adds routes background

--- src/index.js
+++ src/index.js
@@ -1,95 +1,39 @@
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./Home";
+import PostNew from "./PostNew";
+import PostEdit from "./PostEdit";
+import PostIndex from "./PostIndex";
+import PostShow from "./PostShow";
+import About from "./About";
+import Login from "./Login";
 
-function App() {
+export default function App() {
   return (
-    <div id="root">
-      <div class="alert alert-success alert-dismissible fade show" role="alert" v-if="isLoggedIn()">
-        <strong>Successfully Logged In!</strong>
-        You are free to make posts now.
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-      </div>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="/">
-            Home
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/posts">
-                  All Posts
-                </a>
-              </li>
-              <li class="nav-item" v-if="!isLoggedIn()">
-                <a class="nav-link" href="/login">
-                  Login
-                </a>
-              </li>
-              <li class="nav-item" v-if="isLoggedIn()">
-                <a class="nav-link" href="/logout">
-                  Logout
-                </a>
-              </li>
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li>
-                    <a class="dropdown-item" v-if="isLoggedIn()" href="/posts/new">
-                      Create Post
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="/about">
-                      About
-                    </a>
-                  </li>
-                  <div v-if="!isLoggedIn()">
-                    <li>
-                      <hr class="dropdown-divider" />
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="/signup">
-                        Sign Up
-                      </a>
-                    </li>
-                  </div>
-                </ul>
-              </li>
-              <li class="nav-item" v-if="!isLoggedIn()">
-                <a class="nav-link disabled">Welcome Guest</a>
-              </li>
-            </ul>
-            <form class="d-flex">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button class="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
-          </div>
-        </div>
-      </nav>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="posts/new" element={<PostNew />} />
+          <Route path="about" element={<About />} />
+          <Route path="login" element={<Login />} />
+          <Route path="posts/edit" element={<PostEdit />} />
+          <Route path="posts/index" element={<PostIndex />} />
+          <Route path="posts/show" element={<PostShow />} />
+          <Route path="posts/index" element={<PostIndex />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App();
+const myfirstelement = <h1>Hello React!</h1>;
+function Hello() {
+  return <h2>Hello World!</h2>;
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(myfirstelement, document.getElementById("mydiv"));
+ReactDOM.render(<Hello />, document.getElementById("root"));
