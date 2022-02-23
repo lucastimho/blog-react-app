e3cc690aae4d2699435ab58d62440bbaee723075
lucastimho <lucas.t.ho.lh@gmail.com>
2022-02-22 19:59:55 -0600

    reformats to display alert more correctly

--- src/Layout.js
+++ src/Layout.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import "./index.js";
+// import "./index.js";
 import "./Layout.css";
 // import { Outlet, Link } from "react-router-dom";
 
@@ -12,176 +12,24 @@ function isLoggedIn() {
   }
 }
 
-const loginAlert = () => {
-  if (isLoggedIn()) {
-    return (
-      <>
-        <div class="alert alert-success alert-dismissible fade show">
-          <strong>Sucessfully Logged In!</strong>
-          You are free to make posts now.
-          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-      </>
-    );
-  }
-};
-
-const Layout = () => {
-  if (isLoggedIn()) {
-    return <UserNavBar />;
-  } else {
-    return <GuestNavBar />;
-  }
-};
-
-function UserNavBar() {
+const LoginAlert = () => {
   return (
-    <>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="/">
-            Home
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="navbar-toggler-icon"></div>
-        </div>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/posts">
-                All Posts
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/logout">
-                Logout
-              </a>
-            </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li>
-                  <a class="dropdown-item" href="/posts/new">
-                    Create Post
-                  </a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="/about">
-                    About
-                  </a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-          <form class="d-flex">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button class="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
-        </div>
-      </nav>
-    </>
-  );
+    <div className="alert alert-success alert-dismissible fade show" role="alert">
+      {isLoggedIn = true &&
+      <>
+        <strong>Successfully Logged In!</strong>
+        You are free to make posts now.
+        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </>
+      }
+    </div>
+  )
 }
 
-function GuestNavBar() {
-  return (
-    <>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="/">
-            Home
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/posts">
-                  All Posts
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/login">
-                  Login
-                </a>
-              </li>
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li>
-                    <a class="dropdown-item" href="/about">
-                      About
-                    </a>
-                  </li>
-                  <div>
-                    <li>
-                      <hr class="dropdown-divider" />
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="/signup">
-                        Sign Up
-                      </a>
-                    </li>
-                  </div>
-                </ul>
-              </li>
-              <li class="nav-item" v-if="!isLoggedIn()">
-                <a class="nav-link disabled">Welcome Guest</a>
-              </li>
-            </ul>
-            <form class="d-flex">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button class="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
-          </div>
-        </div>
-      </nav>
-    </>
-  );
+class Layout extends Component {
+  render ()
 }
 
 export default Layout;
 
-ReactDOM.render(loginAlert, document.getElementById("alert"));
 ReactDOM.render(<Layout />, document.getElementById("navbar"));
